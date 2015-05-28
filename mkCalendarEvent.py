c07032d9e6a04108129529bebc6c912b38edfd7c
Dave Jagoda <davejagoda@gmail.com>
2015-05-28 13:31:27 -0700

    require tokenFile argument in mkCalendar.py

--- mkCalendarEvent.py
+++ mkCalendarEvent.py
@@ -22,10 +22,9 @@ def getTimeZoneOffSet():
         sign = '+'
     return('{}{:02d}:{:02d}'.format(sign,hh,mm))
 
-def get_calendar_service():
-    f = open('bearer_token.json', 'r')
-    credentials = oauth2client.client.Credentials.new_from_json(f.read())
-    f.close()
+def get_calendar_service(tokenFile):
+    with open(tokenFile, 'r') as f:
+        credentials = oauth2client.client.Credentials.new_from_json(f.read())
     http = httplib2.Http()
     credentials.authorize(http)
     return(apiclient.discovery.build('calendar', 'v3', http=http))
@@ -70,8 +69,10 @@ if '__main__' == __name__:
     parser.add_argument('--hours', help='how many hours from now to start the event')
     parser.add_argument('--start', help='start time of the event: CCYY-MM-DDTHH:MM:SS')
     parser.add_argument('--end', help='end time of the event: CCYY-MM-DDTHH:MM:SS')
+    parser.add_argument('-t', '--tokenFile', action='store', required=True, help='file containing OAuth token in JSON format')
+
     args = parser.parse_args()
-    calendar_service = get_calendar_service()
+    calendar_service = get_calendar_service(args.tokenFile)
     if args.hours:
         startTime = (datetime.datetime.now() + datetime.timedelta(hours=int(args.hours))).replace(microsecond=0).isoformat()
         print(startTime)
